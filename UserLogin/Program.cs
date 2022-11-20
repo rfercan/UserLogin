8abd662cd777fcf264ab47324fd5dcb2df680be6
Fatih <r.fatihercan@hotmail.com>
2022-11-20 22:21:24 +0300

    Add project files.

---
+++ UserLogin/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string userName;
+int password;
+Console.WriteLine("Enter the username");
+userName = Console.ReadLine();
+Console.WriteLine("Enter the password") ;
+password = Convert.ToInt32(Console.ReadLine());
+
+if (userName=="admin" && password==12345)
+{
+    Console.WriteLine("Successful");
+}
+else
+{
+    Console.WriteLine("Error");
+}
